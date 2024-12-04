e6bdce55aa249cb1d4fc8e95586927178f6f5550
Bruno <brn.milano@hotmail.com>
2024-12-04 13:56:18 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./styles/global.css";
+
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
